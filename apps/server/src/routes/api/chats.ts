9966c9017e69bc9b7985786afd788c2f09bf9231
Himanshu <himanshu620434@gmail.com>
2026-03-10 21:17:22 +0530

    feat: implement authentication and chat routes

---
+++ apps/server/src/routes/api/chats.ts
@@ -0,0 +1,12 @@
+import { Router } from "express";
+
+const chatRouter = Router();
+
+/*
+  chat department
+  
+*/
+
+
+
+export default chatRouter;
---
+++ apps/server/src/routes/api/users.ts
@@ -0,0 +1,12 @@
+import { Router } from "express";
+
+const userRouter = Router();
+
+/*
+  users department
+  
+*/
+
+
+
+export default userRouter;
